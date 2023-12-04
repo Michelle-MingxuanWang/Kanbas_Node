709308a285ecb09b5bba1acc513a3593691ca4d5
Michelle-MingxuanWang <mingxuan.wang@outlook.com>
2023-12-03 17:56:53 -0800

    add atlas connection

--- app.js
+++ app.js
@@ -9,14 +9,15 @@ import cors from "cors";
 import mongoose from "mongoose";
 import UserRoutes from "./users/routes.js";
 import session from "express-session";
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+const CONNECTION_STRING = "mongodb+srv://michellemingxuanwang:web5610@cluster0.gqqgjpa.mongodb.net/?retryWrites=true&w=majority" || 'mongodb://127.0.0.1:27017/kanbas'
+mongoose.connect(CONNECTION_STRING);
 
 
 const app = express();
 app.use(
     cors({
       credentials: true,
-      origin: "http://localhost:3000",
+      origin: process.env.FRONTEND_URL
     })
    );  
    
@@ -24,10 +25,17 @@ const sessionOptions = {
     secret: "any string",
     resave: false,
     saveUninitialized: false,
-  };
-  app.use(
-    session(sessionOptions)
-  );
+};
+
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+    };
+}
+
+app.use(session(sessionOptions));
 
   
 app.use(express.json());
